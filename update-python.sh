a5c167b7d360f10b270e01005948b837be21cb49
Saggi Mizrahi <saggi.mizrahi@huawei.com>
2017-03-06 07:35:06 +0100

    Add python venv update script

---
+++ update-python.sh
@@ -0,0 +1,6 @@
+#!/bin/env sh
+cd ~/.config/nvim
+pyenv3/bin/pip install --upgrade pip
+pyenv3/bin/pip install --upgrade neovim
+pyenv2/bin/pip install --upgrade pip
+pyenv2/bin/pip install --upgrade neovim
